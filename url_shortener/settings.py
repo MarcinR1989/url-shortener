87d0dcd7947bc710ef66cfcd881a40a053b9c17d
MarcinR1989 <rykaczewskim@gmail.com>
2021-02-25 20:59:07 +0100

    added collect static

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -133,9 +133,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 DATABASE_URL = os.environ.get('DATABASE_URL')
 
