7b21ce06d44631b482ecabccc14a99d281dd9d20
MarcinR1989 <rykaczewskim@gmail.com>
2021-02-25 19:36:35 +0100

    delete sqlite3

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -30,7 +30,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://git.heroku.com/url-shortener-marcin.git', '127.0.0.1']
+ALLOWED_HOSTS = [
+    'https://git.heroku.com/url-shortener-marcin.git',
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -81,12 +84,12 @@ WSGI_APPLICATION = 'url_shortener.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
