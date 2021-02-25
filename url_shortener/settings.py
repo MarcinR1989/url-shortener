44c6570c17fe9252872e1a43d0dd8d34f8e31baf
MarcinR1989 <rykaczewskim@gmail.com>
2021-02-25 18:20:04 +0100

    connecting to db

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://git.heroku.com/url-shortener-marcin.git']
+ALLOWED_HOSTS = ['https://git.heroku.com/url-shortener-marcin.git', '127.0.0.1']
 
 
 # Application definition
